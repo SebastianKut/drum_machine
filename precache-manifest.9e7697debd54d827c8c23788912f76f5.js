6a2e8aafb9878a2e8648e6c8270b28b88ade43de
sebastiankut <sebastian_kut@yahoo.co.uk>
2020-09-11 22:12:30 +0200

    Updates

---
+++ precache-manifest.9e7697debd54d827c8c23788912f76f5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "082db715032ccbfd762cba09d1355d8f",
+    "url": "/drum_machine/index.html"
+  },
+  {
+    "revision": "b0b64c8bcc70d0d1e3f7",
+    "url": "/drum_machine/static/css/main.ae8841c4.chunk.css"
+  },
+  {
+    "revision": "7c227d6ea3eeaae11765",
+    "url": "/drum_machine/static/js/2.c8e36470.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/drum_machine/static/js/2.c8e36470.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0b64c8bcc70d0d1e3f7",
+    "url": "/drum_machine/static/js/main.b9b3740d.chunk.js"
+  },
+  {
+    "revision": "d129b22bb7a1283ece0c",
+    "url": "/drum_machine/static/js/runtime-main.d5a7a7e7.js"
+  }
+]);
